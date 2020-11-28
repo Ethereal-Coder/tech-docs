8cad6eba7328650918e80938b22a2c7aa8e2d062
Ethereal-Coder <Ethereal-Coder@users.noreply.github.com>
2020-11-28 16:22:15 +0000

    Automated deployment: Sat Nov 28 16:22:15 UTC 2020 c6f20e404590f7992db11d48280df9548c9eb174

--- service-worker.js
+++ service-worker.js
@@ -27,19 +27,19 @@ self.addEventListener('message', (event) => {
 self.__precacheManifest = [
   {
     "url": "404.html",
-    "revision": "dbe15ddc1e393dd22a353e6456aad442"
+    "revision": "6dcc46a428b18c5c36e2f8437be0f601"
   },
   {
     "url": "Argo_Workflow.html",
-    "revision": "20d0bbb38167e9d9df38694a2de31205"
+    "revision": "d2d074c977d6ce2cdac303848ba8e0f5"
   },
   {
-    "url": "assets/css/0.styles.06e3185a.css",
-    "revision": "f8c276d7d5300ff366e6b1fce2e31402"
+    "url": "assets/css/0.styles.5e647402.css",
+    "revision": "b11fd613eff880f99469fb44a1ed1dc2"
   },
   {
-    "url": "assets/img/bg.a2d1cf1a.svg",
-    "revision": "a2d1cf1ad2becdd79823541dae101fcd"
+    "url": "assets/img/bg.2cfdbb33.svg",
+    "revision": "2cfdbb338a1d44d700b493d7ecbe65d3"
   },
   {
     "url": "assets/img/image-20200415163042515.3fd1575d.png",
@@ -50,8 +50,8 @@ self.__precacheManifest = [
     "revision": "f970983796d70cc486c199efcc334dd6"
   },
   {
-    "url": "assets/js/1.e8bc09c6.js",
-    "revision": "65e0d53f29807554dc4112aaea193d11"
+    "url": "assets/js/1.6fc172ed.js",
+    "revision": "1b28d0ff7bbf173efecf8c28cef7cc84"
   },
   {
     "url": "assets/js/10.3ba13620.js",
@@ -62,31 +62,31 @@ self.__precacheManifest = [
     "revision": "c212b097a37eeeda26de660733257310"
   },
   {
-    "url": "assets/js/12.6d0b546a.js",
-    "revision": "20f639cb9b7394c22b755b0ce043a870"
+    "url": "assets/js/12.7ea2c620.js",
+    "revision": "d67acbc47b67133bfaee77236b77fbe4"
   },
   {
     "url": "assets/js/13.8489a32d.js",
     "revision": "e09a2b4ac8acf5f31a0d1d5b015a2311"
   },
   {
-    "url": "assets/js/14.1b3977aa.js",
-    "revision": "21e5caaa8ad084a470575d26c0989e8f"
+    "url": "assets/js/14.3e4cc1c9.js",
+    "revision": "ad7c64d89cbb7bc060c4462d73c164f0"
   },
   {
     "url": "assets/js/15.63b0aba9.js",
     "revision": "101cd72d9f3e06cb1e4b3d0978fd2b8a"
   },
   {
-    "url": "assets/js/3.f87dea88.js",
-    "revision": "3262593db1986be91d8a64078cedd4a0"
+    "url": "assets/js/3.5516c88a.js",
+    "revision": "f94f16dded85ade93217ccbd2e92010c"
   },
   {
-    "url": "assets/js/4.cf45ce49.js",
+    "url": "assets/js/4.38765461.js",
     "revision": "75613ea1c114dcd4bb0ba8aa5142d2d3"
   },
   {
-    "url": "assets/js/5.34c6c211.js",
+    "url": "assets/js/5.20afb687.js",
     "revision": "3cea53867a2d8d07c7980e0dd6d004a9"
   },
   {
@@ -106,8 +106,8 @@ self.__precacheManifest = [
     "revision": "5ea573e587c54d98b38475ca8cb1135c"
   },
   {
-    "url": "assets/js/app.a67fab7b.js",
-    "revision": "783a1085c7f876fc23cba36c96b627d5"
+    "url": "assets/js/app.5803eb0d.js",
+    "revision": "a3ed0ca2c8aff7e84e07dcc72d67e414"
   },
   {
     "url": "avatar.png",
@@ -123,27 +123,43 @@ self.__precacheManifest = [
   },
   {
     "url": "categories/Argo/index.html",
-    "revision": "1aa64ac511624c1806c525610218ac15"
+    "revision": "03d086736b82bb87ba4de4795e9347e6"
   },
   {
     "url": "categories/DevOps/index.html",
-    "revision": "9fe0767accfaedd9cf45542d3dbc03e3"
+    "revision": "867d4c168dc4b760230ff22c798149fa"
   },
   {
     "url": "categories/index.html",
-    "revision": "700bae1543eba4d6d68ecd022cc85650"
+    "revision": "30b0a11e1ade99a9c3c7a55b5ceb6f88"
+  },
+  {
+    "url": "go.png",
+    "revision": "25b723d25a94b1ee34d7c80cdb313adc"
+  },
+  {
+    "url": "Golang-Advantage.jpg",
+    "revision": "3d7414a878f27daec955387611487052"
+  },
+  {
+    "url": "goo.png",
+    "revision": "a3336b4c3a5e9a1d8e2f32be8dab2db5"
+  },
+  {
+    "url": "gooo.png",
+    "revision": "51760db1844908d4e03806cca519d133"
   },
   {
     "url": "index.html",
-    "revision": "9222953b43a1960167b7db1948fa184e"
+    "revision": "41d1a3da1e86c6460184472bebe5df28"
   },
   {
     "url": "js/MouseClickEffect.js",
-    "revision": "0098b6a05157f24df2b894b7afeeeab2"
+    "revision": "a46aba34b3ce7a33aca347ae31606056"
   },
   {
-    "url": "live2d/koharu/assets/moc/koharu.2048/texture_00.png",
-    "revision": "495eea8d906c2b03abfe3fa9de2f2a8b"
+    "url": "live2d/haruto/assets/moc/haruto.2048/texture_00.png",
+    "revision": "56ff69b411abfc80cb68d0b1267959c5"
   },
   {
     "url": "logo.png",
@@ -151,15 +167,15 @@ self.__precacheManifest = [
   },
   {
     "url": "tag/Argo/index.html",
-    "revision": "55a2226269cc1fb4a70d39e015153221"
+    "revision": "a0e0870858edd270eac69b9ae0633ff4"
   },
   {
     "url": "tag/index.html",
-    "revision": "cd21fec419e30ce3494d5909193be1a9"
+    "revision": "ec5625e9bc300e5c75d3f3bafd2b37ca"
   },
   {
     "url": "timeline/index.html",
-    "revision": "af93baecae08164c9acd77662425b37c"
+    "revision": "7056fd8d899e590293e109ff7ff5df5d"
   }
 ].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
